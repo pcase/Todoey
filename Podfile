6acc8fb5b26ea980f69ac12518aa75cdcde43e31
Patty Case <beadypatty@yahoo.com>
2018-10-16 17:42:51 -0700

    Updated pods, changed app icons

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Todoey' do
+  use_frameworks!
+
+  # Pods for Todoey
+
+pod 'RealmSwift'
+pod 'SwipeCellKit'
+pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
+end
---
+++ build/Pods.build/Debug-iphoneos/Pods-Todoey.build/DerivedSources/Pods_Todoey_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_TodoeyVersionString[];
+ extern const double Pods_TodoeyVersionNumber;
+
+ const unsigned char Pods_TodoeyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Todoey  PROJECT:Pods-1" "\n";
+ const double Pods_TodoeyVersionNumber __attribute__ ((used)) = (double)1.;
